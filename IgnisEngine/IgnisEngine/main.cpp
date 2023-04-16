d27bfbd3474343eac40842d1ece9d28fd6038654
Mickey Ordog <mickeyordog@gmail.com>
2023-04-15 18:11:11 -0700

    Configure support for MacOS

--- IgnisEngine/IgnisEngine/main.cpp
+++ IgnisEngine/IgnisEngine/main.cpp
@@ -9,18 +9,30 @@ and may not be redistributed without written permission.*/
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+//Starts up SDL and creates window
 bool init();
+
+//Loads media
 bool loadMedia();
+
+//Frees media and shuts down SDL
 void close();
 
+//The window we'll be rendering to
 SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
 SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
 SDL_Surface* gHelloWorld = NULL;
 
 bool init()
 {
+	//Initialization flag
 	bool success = true;
 
+	//Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
@@ -28,6 +40,7 @@ bool init()
 	}
 	else
 	{
+		//Create window
 		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (gWindow == NULL)
 		{
@@ -36,6 +49,7 @@ bool init()
 		}
 		else
 		{
+			//Get window surface
 			gScreenSurface = SDL_GetWindowSurface(gWindow);
 		}
 	}
@@ -61,45 +75,47 @@ bool loadMedia()
 
 void close()
 {
+	//Deallocate surface
 	SDL_FreeSurface(gHelloWorld);
 	gHelloWorld = NULL;
 
+	//Destroy window
 	SDL_DestroyWindow(gWindow);
 	gWindow = NULL;
 
+	//Quit SDL subsystems
 	SDL_Quit();
 }
 
 int main(int argc, char* args[])
 {
+	//Start up SDL and create window
 	if (!init())
 	{
 		printf("Failed to initialize!\n");
 	}
 	else
 	{
+		//Load media
 		if (!loadMedia())
 		{
 			printf("Failed to load media!\n");
 		}
 		else
 		{
-			bool quit = false;
-			SDL_Event e;
-			while (!quit)
-			{
-				while (SDL_PollEvent(&e) != 0)
-				{
-					if (e.type == SDL_QUIT)
-					{
-						quit = true;
-					}
-				}
-				SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
-				SDL_UpdateWindowSurface(gWindow);
-			}
+			//Apply the image
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+
+			//Update the surface
+			SDL_UpdateWindowSurface(gWindow);
+
+			//Hack to get window to stay up
+			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
 		}
 	}
+
+	//Free resources and close SDL
 	close();
+
 	return 0;
 }
