648befaa5aefec9b047f98ce8fb4a21917b28aa7
Mickey Ordog <mickeyordog@gmail.com>
2023-05-02 02:49:14 -0700

    Fix subdirectory header error, add input handling

--- src/visual/gameObject.cpp
+++ src/visual/gameObject.cpp
@@ -1,10 +1,10 @@
 #include <SDL.h>
 #include "gameObject.h"
 #include "renderer.h"
-// #include "inputHandler.h"
+#include "inputHandler.h"
 
 // Makes me kinda nervous that it's keeping a pointer to the sprite
-GameObject::GameObject(Sprite* sprite, int x, int y, int width, int height) : x(x), y(y), width(width), height(height)
+GameObject::GameObject(Sprite* sprite, float x, float y, int width, int height) : x(x), y(y), width(width), height(height)
 {
     this->sprite = sprite;
 }
@@ -16,9 +16,10 @@ GameObject::~GameObject()
 
 void GameObject::update(float dt)
 {
-    // x += InputHandler::getInstance().queryKeyPressed(SDLK_d) ? 10 * dt : 0;
-    x = 100 + 100 * cos(SDL_GetTicks() / 1000.0);
-    y = 100 + 100 * sin(SDL_GetTicks() / 1000.0);
+    x += InputHandler::getInstance().queryKeyPressed(SDLK_d) ? 1000 * dt : 0;
+    x -= InputHandler::getInstance().queryKeyPressed(SDLK_a) ? 1000 * dt : 0;
+    // x = 100 + 100 * cos(SDL_GetTicks() / 1000.0);
+    // y = 100 + 100 * sin(SDL_GetTicks() / 1000.0);
 }
 
 void GameObject::render(Renderer* renderer)
--- src/visual/gameObject.h
+++ src/visual/gameObject.h
@@ -4,15 +4,15 @@
 
 class GameObject {
 public:
-    GameObject(Sprite* sprite, int x, int y, int width, int height);
+    GameObject(Sprite* sprite, float x, float y, int width, int height);
     ~GameObject();
     void update(float dt);
     void render(Renderer* renderer);
 
 private:
     Sprite* sprite;
-    int x;
-    int y;
+    float x;
+    float y;
     int width;
     int height;
 };
