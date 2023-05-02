5a54b6f3cbd418a9c39d6e81a9f16ed1cc940c97
Mickey Ordog <mickeyordog@gmail.com>
2023-05-02 02:49:39 -0700

    Update gameObject.cpp

--- src/visual/gameObject.cpp
+++ src/visual/gameObject.cpp
@@ -16,6 +16,7 @@ GameObject::~GameObject()
 
 void GameObject::update(float dt)
 {
+    // TODO: this causes weird behavior on hold down
     x += InputHandler::getInstance().queryKeyPressed(SDLK_d) ? 1000 * dt : 0;
     x -= InputHandler::getInstance().queryKeyPressed(SDLK_a) ? 1000 * dt : 0;
     // x = 100 + 100 * cos(SDL_GetTicks() / 1000.0);
