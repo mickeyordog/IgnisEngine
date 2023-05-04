332aa34bc58d45fd0deac7b33634644b03255032
Mickey Ordog <mickeyordog@gmail.com>
2023-05-04 00:39:38 -0700

    Work on debugging mono not linking

--- src/monoTest.c
+++ src/monoTest.c
@@ -4,5 +4,7 @@
 int main( int argc, char* args[] ) {
     MonoDomain *domain;
 
-    domain = mono_jit_init (domain_name);
+    domain = mono_jit_init ("test.cs");
 }
+// are we looking for mono-2.0.a? or something else like .dylib
+// if we need the .dylib, how could I fix that file universal problem?
