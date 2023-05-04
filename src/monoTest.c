d84626fde55b39432411661ab4768fc507b11e5a
Mickey Ordog <mickeyordog@gmail.com>
2023-05-04 15:33:55 -0700

    Think I got Mono working with manual clang invoke?

--- src/monoTest.c
+++ src/monoTest.c
@@ -5,6 +5,9 @@ int main( int argc, char* args[] ) {
     MonoDomain *domain;
 
     domain = mono_jit_init ("test.cs");
+    printf("Worked?\n");
 }
 // are we looking for mono-2.0.a? or something else like .dylib
 // if we need the .dylib, how could I fix that file universal problem?
+
+// clang src/monoTest.c `pkg-config --cflags --libs mono-2` -o test
