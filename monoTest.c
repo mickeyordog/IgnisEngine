8f6268ce37a795d92190570b42ee5b76f1ab6514
Mickey Ordog <mickeyordog@gmail.com>
2023-05-04 21:25:46 -0700

    Scripting Mono progress

--- monoTest.c
+++ monoTest.c
@@ -3,12 +3,34 @@
 #include <mono/metadata/debug-helpers.h>
 #include <mono/metadata/mono-config.h>
 
+typedef int (*GetNumber) (int, MonoException**);
+
+// gint32 HashCode(MonoObject *obj) {
+//     return 0;
+// }
+
+// on Windows this needs __dllexport
 void DoSomething() {
     printf("Hello from C!\n");
 }
 
+int PassInt(int i) {
+    return i * 2;
+}
+
+char* PassString(char* str) {
+    return str;
+}
+
+MonoObject* createObject(MonoDomain* domain, MonoImage* image, const char* namespace, const char* className) {
+    MonoClass *my_class = mono_class_from_name (image, namespace, className);
+    MonoObject *my_class_instance = mono_object_new (domain, my_class);
+    mono_runtime_object_init (my_class_instance);
+}
+
 int main( int argc, char* argv[] ) {
     unsetenv("TERM"); // also needed this
+    // remember to set env var for pkg config path too, prob do that in code
     // didn't need this
     // mono_set_dirs ("/Library/Frameworks/Mono.framework/Versions/6.12.0/lib", "/Library/Frameworks/Mono.framework/Versions/6.12.0/etc");
     mono_set_assemblies_path("/Library/Frameworks/Mono.framework/Versions/6.12.0/lib"); // this was what fixed it, had wrong path
@@ -24,10 +46,9 @@ int main( int argc, char* argv[] ) {
     if (!assembly)
         printf("Assembly not found.\n");
 
-    /*
-    MonoImage* image;
-	image = mono_assembly_get_image(assembly);
+    
 
+    /*
     //Build a method description object
     MonoMethodDesc* TypeMethodDesc;
     char* TypeMethodDescStr = "Dog:Type()";
@@ -58,11 +79,43 @@ int main( int argc, char* argv[] ) {
     char* h = "test.exe";
     int retval = mono_jit_exec (domain, assembly, 1, &h);
     printf("retval: %d\n", retval);
+
+    MonoImage* image;
+	image = mono_assembly_get_image(assembly);
+
+
+    // MonoMethodDesc* methodDesc = mono_method_desc_new ("HelloWorld:GetNumber(int)", 0);
+    // if (!methodDesc)
+    //     printf("mono_method_desc_new failed\n");
+    // MonoMethod* method = mono_method_desc_search_in_image(methodDesc, image);
+    // if (!method)
+    //     printf("mono_method_desc_search_in_image failed\n");
+
+    // last param of typedef must be MonoException**!
+    // GetNumber getNumber = mono_method_get_unmanaged_thunk(method);
+    // MonoException* exception;
+    // printf("getNumber(): %d\n", getNumber(5, &exception));
+    // printf("getNumber()");
+    /* we execute methods that take one argument */
+    // void *args [1];
+    // int val = 10;
+    /* Note we put the address of the value type in the args array */
+    // args [0] = &val;
+    // This is alright but unmanaged to managed thunks is better longterm
+    // MonoObject *result = mono_runtime_invoke (method, NULL, args, NULL);
+    /* we always get a MonoObject* from mono_runtime_invoke (), so to get
+    * the integer value we need to unbox (which returns a pointer to
+    * the value stored in the object) and dereference.
+    */
+    // int int_result = *(int*)mono_object_unbox (result);
+    // printf("GetNumber(%d): %d\n", val, int_result);
+
     mono_jit_cleanup (domain);
-    printf("End\n");
+    // printf("End\n");
 }
 // are we looking for mono-2.0.a? or something else like .dylib
 // if we need the .dylib, how could I fix that file universal problem?
 
 // clang src/monoTest.c `pkg-config --cflags --libs mono-2` -o test
 // try setting path to pkg-config of the one in root mac directory, that might've been the dif that fixed it when I went from cmake to console
+// clang monoTest.c -rdynamic `pkg-config --cflags --libs mono-2` -o test
--- src/test.cs
+++ src/test.cs
@@ -5,13 +5,39 @@ using System.Runtime.InteropServices;
 
 public class HelloWorld
 {
-    [DllImport ("__Internal", EntryPoint="DoSomething")]
+    [DllImport ("__Internal", EntryPoint=nameof(DoSomething))]
     static extern void DoSomething ();
-    
+
+    [DllImport ("__Internal", EntryPoint=nameof(PassInt))]
+    static extern int PassInt (int i);
+
+    [DllImport ("__Internal", EntryPoint=nameof(PassString))]
+    static extern string PassString (string s);
+
     public static void Main(string[] args)
     {
-        DoSomething();
-        Console.WriteLine ("Hello Mono World");
+        // DoSomething();
+        // Console.WriteLine($"PassInt(3): {PassInt(3)}");
+        // Console.WriteLine($"PassString(\"Hello\"): {PassString("Hello")}");
+        // Console.WriteLine ("Hello Mono World");
+    }
+
+    public static int GetNumber(int num)
+    {
+        return num * 5;
+    }
+}
+
+public class GameObject {
+    public string name;
+    public GameObject() {
+        this.name = "No name";
+    }
+    public GameObject(string name) {
+        this.name = name;
+    }
+    public void Update(float dt) {
+        Console.WriteLine($"{this.name} updating by {dt} seconds");
     }
 }
 // public class Dog
