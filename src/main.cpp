7487f4b36f6e53c69f2b3b4218cd00957a10bfdb
Mickey Ordog <mickeyordog@gmail.com>
2023-05-25 02:38:34 -0700

    Begin embedding Python

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
 #include <stdio.h>
 #include <iostream>
 #include <imgui.h>
@@ -13,6 +16,98 @@
 #include "texture.h"
 #include "geometry.h"
 
+int main(int argc, char *argv[])
+{
+	PyObject *pName, *pModule, *pFunc;
+	PyObject *pArgs, *pValue;
+	int i;
+
+	if (argc < 3)
+	{
+		fprintf(stderr, "Usage: call pythonfile funcname [args]\n");
+		return 1;
+	}
+
+	Py_Initialize();
+	
+	PyObject *sys = PyImport_ImportModule("sys");
+	PyObject *path = PyObject_GetAttrString(sys, "path");
+	PyList_Append(path, PyUnicode_FromString("."));
+
+	pName = PyUnicode_DecodeFSDefault(argv[1]);
+	if (pName == NULL)
+	{
+		fprintf(stderr, "Error decoding filename\n");
+		return 1;
+	}
+
+	pModule = PyImport_Import(pName);
+	if (pModule == NULL) {
+		PyErr_Print();
+		fprintf(stderr, "Error importing module\n");
+		return 1;
+	}
+	Py_DECREF(pName);
+
+	if (pModule != NULL)
+	{
+		pFunc = PyObject_GetAttrString(pModule, argv[2]);
+		/* pFunc is a new reference */
+
+		if (pFunc && PyCallable_Check(pFunc))
+		{
+			pArgs = PyTuple_New(argc - 3);
+			for (i = 0; i < argc - 3; ++i)
+			{
+				pValue = PyLong_FromLong(atoi(argv[i + 3]));
+				if (!pValue)
+				{
+					Py_DECREF(pArgs);
+					Py_DECREF(pModule);
+					fprintf(stderr, "Cannot convert argument\n");
+					return 1;
+				}
+				/* pValue reference stolen here: */
+				PyTuple_SetItem(pArgs, i, pValue);
+			}
+			pValue = PyObject_CallObject(pFunc, pArgs);
+			Py_DECREF(pArgs);
+			if (pValue != NULL)
+			{
+				printf("Result of call: %ld\n", PyLong_AsLong(pValue));
+				Py_DECREF(pValue);
+			}
+			else
+			{
+				Py_DECREF(pFunc);
+				Py_DECREF(pModule);
+				PyErr_Print();
+				fprintf(stderr, "Call failed\n");
+				return 1;
+			}
+		}
+		else
+		{
+			if (PyErr_Occurred())
+				PyErr_Print();
+			fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
+		}
+		Py_XDECREF(pFunc);
+		Py_DECREF(pModule);
+	}
+	else
+	{
+		PyErr_Print();
+		fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
+		return 1;
+	}
+	if (Py_FinalizeEx() < 0)
+	{
+		return 120;
+	}
+	return 0;
+}
+
 // TODO: extension that lets you add includes more easily
 // TODO: fix window resizing
 // TODO: allow editing gameobject fields in editor
@@ -20,8 +115,22 @@
 // TODO: set up scene editor
 // TODO: fancier scene rendering, eg 3D stuff
 // TODO: script embed/implementation
-int main(int argc, char* args[])
+int main2(int argc, char* args[])
 {
+	Py_Initialize();
+	// PyRun_SimpleString("print('Hello from Python!')");
+	// Py_DecodeLocale()
+	FILE* file = fopen("../src/script/test.py", "rb");
+	if (PyRun_SimpleFileExFlags(file, "test.py", true, nullptr) < 0)
+	{
+		std::cout << "PyRun_SimpleFile() failed" << std::endl;
+	}
+	if (Py_FinalizeEx() < 0)
+	{
+		std::cout << "Py_FinalizeEx() failed" << std::endl;
+	}
+	return 0;
+
 	SDLContext sdlContext("Ignis Engine", 1280, 720);
 	GLContext glContext(&sdlContext);
 	DearImGuiContext dearImGuiContext(&sdlContext, &glContext);
